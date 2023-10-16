8e16186eea145430adaf008e355a0a80bf2274b1
hyesungpark <jayhspark@gmail.com>
2023-10-16 22:05:37 +0900

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Hye Sung Park on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
